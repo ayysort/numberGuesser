657b5d6b828960b7a3f84c4127dbbbd7f56166ff
ayysort <124464882+ayysort@users.noreply.github.com>
2024-01-23 13:06:46 -0800

    added play again
    just need to polish and then I'm finished!

--- numberGuess.py
+++ numberGuess.py
@@ -7,28 +7,56 @@ print(' ')
 print(' ')
 
 count = 0
+play = True
 
-high = input("I'm thinking of a number between 1 and _?: ")
-number = random.randint(1, int(high))
+while play == True:
+  while True:
+    high = input("I'm thinking of a number between 1 and _?: ")
+    if high.isdigit():
+      break
+    else:
+      print("Wrong Input.. Please type in a number")
+    
+  number = random.randint(1, int(high))
 
-print(' ')
-print('Ok, I got one!')
-print("I'm thinking of a number between 1 and", int(high))
+  print(' ')
+  print('Ok, I got one!')
+  print("I'm thinking of a number between 1 and", int(high))
 
-while True:
+  while True:
+    
+    guess = int(input("Guess the number:  "))
+    
+    count = count + 1
+    
+    if guess == number:
+      print('Correct!')
+      break
+    elif guess > number:
+      print('Lower')
+    elif guess < number:
+      print('Higher')
+    else:
+      print('Wrong Input...')
+
+  print("That took", count, 'attempts.')
   
-  guess = int(input("Guess the number:  "))
   
-  count = count + 1
-  
-  if guess == number:
-    print('Correct!')
+  again = input("Would you like to play again? (y/n): ")
+  if again == 'y':
+    count = 0
+    continue
+  elif again == 'n':
     break
-  elif guess > number:
-    print('Lower')
-  elif guess < number:
-    print('Higher')
   else:
-    print('Wrong Input...')
-
-print("That took", count, 'attempts.')
+    while True:
+      print("Wrong Input...")
+      again2 = input("Would you like to play again? (y/n): ")
+      if again2 == 'y':
+        count = 0
+        break
+      elif again2 == 'n':
+        play = False
+        break
+      else:
+        continue
